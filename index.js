ddcc6c2aa8279718a582e05512f5024b3ec54155
AterixGz <tpsbb123@gmail.com>
2025-04-29 22:55:57 +0700

    polyfill

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+require('web-streams-polyfill');
 const { Client, GatewayIntentBits, Collection, PermissionFlagsBits, AttachmentBuilder  } = require("discord.js");
 const fs = require('fs');
 const path = require('path');
