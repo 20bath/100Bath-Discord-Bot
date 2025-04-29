8ce78aa3bb726c03ca9cf5ee09ff6fc4b94e41fb
AterixGz <tpsbb123@gmail.com>
2025-04-29 23:12:10 +0700

    config firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,7 +2,7 @@ const admin = require('firebase-admin');
 
 const serviceAccount = {
   projectId: process.env.FIREBASE_PROJECT_ID,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  privateKey: process.env.FIREBASE_PRIVATE_KEY,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL
 };
 
